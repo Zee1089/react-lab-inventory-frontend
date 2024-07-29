913d9c55027ff0bd94384285ae73e2359da9a073
jason <jason_tan75@hotmail.com>
2024-07-28 21:06:58 -0400

    changed expiration -> expirationDate to match Schema model, added check for author value to prevent error

--- src/components/ReagentList/ReagentList.jsx
+++ src/components/ReagentList/ReagentList.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const ReagentList = (props) => {
-
+    
     return (
         <main>
             {props.reagents.map((reagent) => (
@@ -12,7 +12,8 @@ const ReagentList = (props) => {
                                 <h2>{reagent.name}</h2>
                             </div>
                             <p>
-                            {reagent.author.username} posted on {new Date(reagent.createdAt).toLocaleDateString()}
+                            {reagent.author ? reagent.author.username : '[ Unknown User ]'} posted on {new Date(reagent.createdAt).toLocaleDateString()}
+                                {/* Added check for author value on reagent object */}
                             </p>
                         </header>
                         {/* <ul>
