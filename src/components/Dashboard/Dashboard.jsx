f841af2842dfdf0b08aaf622d34ac3da2a4a70d7
Zee <zeeboo696@gmail.com>
2024-08-02 05:40:57 -0400

    logo added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ const Dashboard = ({}) => {
       <h1>Welcome, {user.username}</h1>
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
-        of all of your things.
+        of all of your things. Why I cant see the logo.
       </p>
     </main>
     </>
