9e118f9e24ea95d59079ec13d28f6b7fb3059487
Zee <zeeboo696@gmail.com>
2024-08-02 09:31:19 -0400

    Reagent list css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = ({}) => {
       <h1>Welcome, {user.username}</h1>
       <p>
         This is the dashboard page where you, and only you, can see a dashboard
-        of all of your things. Why I cant see the logo.
+        of all of your things. 
       </p>
     </main>
     </>
