43e1438195abb01112ab409ded34a4c363e5d1ba
Zee <zeeboo696@gmail.com>
2024-08-01 21:24:02 -0400

    Reagent list page customized

--- src/components/ReagentList/ReagentList.jsx
+++ src/components/ReagentList/ReagentList.jsx
@@ -6,12 +6,14 @@ const ReagentList = (props) => {
         <main>
             <h1>Reagents</h1>
             <hr />
+            <ul>
             {props.reagents.map((reagent) => (
-                <Link key={reagent._id} to={`/reagents/${reagent._id}`}>
+                <li key = {reagent._id}>
+                <Link to={`/reagents/${reagent._id}`}>
                     <article>
                         <header>
                             <div>
-                                <h2>{reagent.quantity}-{reagent.name}</h2>
+                                <h2>Quantity: {reagent.quantity} Name: {reagent.name}</h2>
                             </div>
                             <p>
                                 {reagent.author ? reagent.author.username : '[ Unknown User ]'} posted on {new Date(reagent.createdAt).toLocaleDateString()}
@@ -19,7 +21,9 @@ const ReagentList = (props) => {
                         </header>
                     </article>
                 </Link>
+                </li>
             ))}
+            </ul>
         </main>
     );
 };
