178cb5f142ce6e6653849f13fe5abbf378ed9781
Zee <zeeboo696@gmail.com>
2024-08-01 21:31:48 -0400

    style

--- src/components/ReagentList/ReagentList.jsx
+++ src/components/ReagentList/ReagentList.jsx
@@ -1,25 +1,26 @@
 import { Link } from 'react-router-dom';
 
 const ReagentList = (props) => {
-
     return (
         <main>
             <h1>Reagents</h1>
             <hr />
-            {props.reagents.map((reagent) => (
-                <Link key={reagent._id} to={`/reagents/${reagent._id}`}>
-                    <article>
-                        <header>
-                            <div>
-                                <h4>{reagent.quantity} </h4>- <h3> {reagent.name}</h3>
-                            </div>
-                            <p>
-                                {reagent.author ? reagent.author.username : '[ Unknown User ]'} posted on {new Date(reagent.createdAt).toLocaleDateString()}
-                            </p>
-                        </header>
-                    </article>
-                </Link>
-            ))}
+            <ul>
+                {props.reagents.map((reagent) => (
+                    <li key={reagent._id}>
+                        <Link to={`/reagents/${reagent._id}`}>
+                            <article>
+                                <header>
+                                    <h3>{reagent.quantity} - {reagent.name}</h3>
+                                    <p>
+                                        {reagent.author ? reagent.author.username : '[ Unknown User ]'} posted on {new Date(reagent.createdAt).toLocaleDateString()}
+                                    </p>
+                                </header>
+                            </article>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 };
