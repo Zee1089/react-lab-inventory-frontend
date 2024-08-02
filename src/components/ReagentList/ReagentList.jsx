5c6f99d0b889b43348fb50ad98eb1e44df74e8cf
Zee <zeeboo696@gmail.com>
2024-08-02 10:22:12 -0400

    Added bullet point

--- src/components/ReagentList/ReagentList.jsx
+++ src/components/ReagentList/ReagentList.jsx
@@ -1,34 +1,30 @@
 import { Link } from 'react-router-dom';
-import './ReagentList.css';
+import './ReagentList.css'
+
 
 const ReagentList = (props) => {
+
     return (
         <main>
             <h2>Reagents</h2>
             <hr />
             <ul>
-                {props.reagents.map((reagent) => (
-                    <li key={reagent._id}>
-                        <Link to={`/reagents/${reagent._id}`}>
-                            <article>
-                                <header>
-                                    <div>
-                                        <h3 >
-                                            {reagent.name}
-                                        </h3>
-                                    </div>
-                                    <p className="reagent-details">
-                                        {reagent.author
-                                            ? reagent.author.username
-                                            : '[ Unknown User ]'}{' '}
-                                        posted on{' '}
-                                        {new Date(reagent.createdAt).toLocaleDateString()}
-                                    </p>
-                                </header>
-                            </article>
-                        </Link>
-                    </li>
-                ))}
+            {props.reagents.map((reagent) => (
+                <li key={reagent._id}>
+                <Link to={`/reagents/${reagent._id}`}>
+                    <article>
+                        <header>
+                            <div>
+                                <h3>  {reagent.name}</h3>
+                            </div>
+                            <p>
+                                {reagent.author ? reagent.author.username : '[ Unknown User ]'} posted on {new Date(reagent.createdAt).toLocaleDateString()}
+                            </p>
+                        </header>
+                    </article>
+                </Link>
+                </li>
+            ))}
             </ul>
         </main>
     );
